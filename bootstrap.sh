624d3029f066cd189654c557d9069075028c9743
Michael Feher <PersonalPC@live.com>
2014-08-23 17:53:47 -0600

    Hmmpf

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "deb http://download.opensuse.org/repositories/security:/OpenVAS:/UNSTABLE:/v6/Debian_7.0/ ./" >> /etc/apt/sources.list
+#wget http://download.opensuse.org/repositories/security:/OpenVAS:/UNSTABLE:/v6/Debian_7.0/Release.key
+#apt-key add ./Release.key
+#apt-get update
+#apt-get -y install couchdb nginx nodejs node greenbone-security-assistant openvas-cli openvas-manager openvas-scanner openvas-administrator sqlite3 xsltproc rsync
+#apt-get -y install texlive-latex-base texlive-latex-extra texlive-latex-recommended htmldoc
+#apt-get install knockd
